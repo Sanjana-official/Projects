c97560bbc03965f5bac41414cecb51ea0352f933
Sanjana <sanjanaofficial879@gmail.com>
2026-04-07 21:58:31 +0530

    Add files via upload

---
+++ projects 42/script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for TOC links
+document.querySelectorAll('.toc a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
