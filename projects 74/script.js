e08c804a280b2a60acd5ff6cfc799ead1886e77f
Sanjana <sanjanaofficial879@gmail.com>
2026-05-09 15:17:08 +0530

    Add files via upload

---
+++ projects 74/script.js
@@ -0,0 +1,14 @@
+// navbar background effect
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 50){
+    navbar.style.background = "rgba(0,0,0,0.5)";
+  }
+  else{
+    navbar.style.background = "rgba(0,0,0,0.2)";
+  }
+
+});
